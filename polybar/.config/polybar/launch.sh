b372026d81f46f0b30d701e8608fd93b7c82ccb3
Sun Howwrongbum <sun@libermatic.com>
2020-06-30 10:09:29 +0530

    feat: dual monitor

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -7,6 +7,12 @@ killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch bar1 and bar2
-polybar bottom &
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar --reload bottom &
+  done
+else
+  polybar --reload bottom &
+fi
 
 echo "Bars launched..."
