08ac11db6e4644aa1eff0ead2684db273b3969bb
Sun Howwrongbum <sun.libermatic.com>
2021-06-22 19:25:50 +0530

    feat(polybar): colors and layout

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
 # Terminate already running bar instances
 killall -q polybar
@@ -6,13 +6,9 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-# Launch bar1 and bar2
-if type "xrandr"; then
-  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-    MONITOR=$m polybar --reload bottom &
-  done
-else
-  polybar --reload bottom &
-fi
+# Launch Polybar, using default config location ~/.config/polybar/config
+echo "---" | tee -a /tmp/polybar-middle.log /tmp/polybar-right.log
+polybar middle 2>&1 | tee -a /tmp/polybar-middle.log & disown
+polybar right 2>&1 | tee -a /tmp/polybar-right.log & disown
 
 echo "Bars launched..."
