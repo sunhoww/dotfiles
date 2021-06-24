d5cef07a2b9ef3c609c1f51c5fa673687b22714f
Sun Howwrongbum <sun.libermatic.com>
2021-06-24 14:14:01 +0530

    fix(ranger): trash

--- ranger/.config/ranger/commands.py
+++ ranger/.config/ranger/commands.py
@@ -1,62 +1,58 @@
-# This is a sample commands.py.  You can add your own commands here.
-#
-# Please refer to commands_full.py for all the default commands and a complete
-# documentation.  Do NOT add them all here, or you may end up with defunct
-# commands when upgrading ranger.
-
-# A simple command for demonstration purposes follows.
-# -----------------------------------------------------------------------------
-
-from __future__ import (absolute_import, division, print_function)
-
-# You can import any python module as needed.
 import os
 
-# You always need to import ranger.api.commands here to get the Command class:
-from ranger.api.commands import Command
+import ranger.api.commands
+from ranger.config.commands import trash as rtrash
+from ranger.container.file import File
+from ranger.container.directory import Directory
 
+class trash(rtrash):
+    """:trash
 
-# Any class that is a subclass of "Command" will be integrated into ranger as a
-# command.  Try typing ":my_edit<ENTER>" in ranger!
-class my_edit(Command):
-    # The so-called doc-string of the class will be visible in the built-in
-    # help that is accessible by typing "?c" inside ranger.
-    """:my_edit <filename>
+    Override of upstream to fix issue #1798 (Ranger crashes when deleting to Trash)
+    Current version v1.9.3. Will be fixed in v1.9.4.
 
-    A sample command for demonstration purposes that opens a file in an editor.
+    TODO: remove after upgrade to v1.9.4
     """
 
-    # The execute method is called when you run this command in ranger.
     def execute(self):
-        # self.arg(1) is the first (space-separated) argument to the function.
-        # This way you can write ":my_edit somefilename<ENTER>".
-        if self.arg(1):
-            # self.rest(1) contains self.arg(1) and everything that follows
-            target_filename = self.rest(1)
+        import shlex
+        from functools import partial
+
+        def is_directory_with_files(path):
+            return os.path.isdir(path) and not os.path.islink(path) and len(os.listdir(path)) > 0
+
+        if self.rest(1):
+            files = shlex.split(self.rest(1))
+            many_files = (len(files) > 1 or is_directory_with_files(files[0]))
         else:
-            # self.fm is a ranger.core.filemanager.FileManager object and gives
-            # you access to internals of ranger.
-            # self.fm.thisfile is a ranger.container.file.File object and is a
-            # reference to the currently selected file.
-            target_filename = self.fm.thisfile.path
+            cwd = self.fm.thisdir
+            tfile = self.fm.thisfile
+            if not cwd or not tfile:
+                self.fm.notify("Error: no file selected for deletion!", bad=True)
+                return
 
-        # This is a generic function to print text in ranger.
-        self.fm.notify("Let's edit the file " + target_filename + "!")
+            files = [f.relative_path for f in self.fm.thistab.get_selection()]
+            many_files = (cwd.marked_items or is_directory_with_files(tfile.path))
 
-        # Using bad=True in fm.notify allows you to print error messages:
-        if not os.path.exists(target_filename):
-            self.fm.notify("The given file does not exist!", bad=True)
-            return
+        confirm = self.fm.settings.confirm_on_delete
+        fileobjs = [_get_object(path) for path in files]
+        if confirm != 'never' and (confirm != 'multiple' or many_files):
+            self.fm.ui.console.ask(
+                "Confirm deletion of: %s (y/N)" % ', '.join(files),
+                partial(self._question_callback, fileobjs),
+                ('n', 'N', 'y', 'Y'),
+            )
+        else:
+            self.fm.execute_file(fileobjs, label='trash')
 
-        # This executes a function from ranger.core.acitons, a module with a
-        # variety of subroutines that can help you construct commands.
-        # Check out the source, or run "pydoc ranger.core.actions" for a list.
-        self.fm.edit_file(target_filename)
 
-    # The tab method is called when you press tab, and should return a list of
-    # suggestions that the user will tab through.
-    # tabnum is 1 for <TAB> and -1 for <S-TAB> by default
-    def tab(self, tabnum):
-        # This is a generic tab-completion function that iterates through the
-        # content of the current directory.
-        return self._tab_directory_content()
+
+        
+def _get_object(path):
+    if not isinstance(path, str):
+        return path
+
+    if os.path.isdir(path):
+        return Directory(path)
+
+    return File(path)
