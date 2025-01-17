73a73533cc9f716032ecfd6e0a0ee4f84543cdf9
Sun Howworngbum <sun@libermatic.com>
2025-01-18 01:44:33 +0530

    feat: add steam

---
+++ 80-gaming.sh
@@ -0,0 +1,9 @@
+# Sat Jan 18 01:36:34 AM IST 2025 - gaming
+
+AddPackage lib32-systemd # system and service manager (32-bit)
+AddPackage steam         # Valve's digital software delivery system
+
+IgnorePath '/usr/lib/gtk-4.0/*'
+IgnorePath '/usr/lib32/gconv/gconv-modules.cache'
+
+CopyFile /etc/sysctl.d/80-gamecompatibility.conf
