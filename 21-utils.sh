3812b91cd93c59e96d34b31ed791875e34aea742
Sun Howworngbum <sun@libermatic.com>
2025-01-18 02:33:40 +0530

    fix: ignored path param

--- 21-utils.sh
+++ 21-utils.sh
@@ -7,7 +7,7 @@ AddPackage wget  # Network utility to retrieve files from the Web
 AddPackage pipewire-alsa  # Low-latency audio/video router and processor - ALSA configuration
 AddPackage pipewire-pulse # Low-latency audio/video router and processor - PulseAudio replacement
 
-IgnorePath /etc/systemd/user/sockets.target.wants/pipewire-pulse.socket /usr/lib/systemd/user/pipewire-pulse.socket
+IgnorePath '/etc/systemd/user/sockets.target.wants/pipewire-pulse.socket'
 
 # bluetooth
 AddPackage bluez       # Daemons for the bluetooth protocol stack
@@ -15,7 +15,7 @@ AddPackage bluez-utils # Development and debugging utilities for the bluetooth p
 
 CreateLink /etc/systemd/system/bluetooth.target.wants/bluetooth.service /usr/lib/systemd/system/bluetooth.service
 
-IgnorePath /etc/systemd/system/dbus-org.bluez.service /usr/lib/systemd/system/bluetooth.service
+IgnorePath '/etc/systemd/system/dbus-org.bluez.service'
 IgnorePath '/var/lib/bluetooth'
 IgnorePath '/var/lib/bluetooth/*'
 IgnorePath '/var/lib/upower/*'
