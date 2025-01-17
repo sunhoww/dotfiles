8adc5d35d22caef4d32931d15a877e76399f673b
Sun Howworngbum <sun@libermatic.com>
2025-01-17 20:30:28 +0530

    feat: add google-chrome

--- 61-apps.sh
+++ 61-apps.sh
@@ -1,3 +1,5 @@
 # Fri Jan 17 07:45:52 PM IST 2025 - graphical apps
 
 AddPackage firefox # Fast, Private & Safe Web Browser
+
+AddPackage --foreign google-chrome # The popular web browser by Google (Stable Channel)
