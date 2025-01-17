f61546871a3189845ecd82c14826808c540583c9
Sun Howworngbum <sun@libermatic.com>
2025-01-17 19:38:16 +0530

    feat: add development apps

---
+++ 60-development.sh
@@ -0,0 +1,13 @@
+# Fri Jan 17 07:18:06 PM IST 2025 - development
+
+AddPackage fish                 # Smart and user friendly shell intended mostly for interactive use
+AddPackage neovim               # Fork of Vim aiming to improve user experience, plugins, and GUIs
+AddPackage starship             # The cross-shell prompt for astronauts
+AddPackage ttc-iosevka          # Typeface family designed for coding, terminal use and technical documents.
+AddPackage ttf-iosevkaterm-nerd # Patched font Iosevka Term from nerd fonts library
+AddPackage fd                   # Simple, fast and user-friendly alternative to find
+AddPackage fzf                  # Command-line fuzzy finder
+AddPackage lazygit              # Simple terminal UI for git commands
+AddPackage ripgrep              # A search tool that combines the usability of ag with the raw speed of grep
+
+IgnorePath '/usr/share/vim/vimfiles/doc/tags'
