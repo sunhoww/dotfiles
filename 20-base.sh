c1546c5cba33931906a23fa6368449b323899215
Sun Howworngbum <sun@libermatic.com>
2025-01-17 18:22:35 +0530

    feat: add reflector

--- 20-base.sh
+++ 20-base.sh
@@ -11,6 +11,7 @@ AddPackage snapper # A tool for managing BTRFS and LVM snapshots. It can create,
 AddPackage vim # Vi Improved, a highly configurable, improved version of the vi text editor
 AddPackage man-db # A utility for reading man pages
 AddPackage man-pages # Linux man pages
+AddPackage reflector # A Python 3 module and script to retrieve and filter the latest Pacman mirror list.
 
 AddPackage --foreign aconfmgr-git # A configuration manager for Arch Linux
 AddPackage --foreign paru # Feature packed AUR helper
@@ -20,18 +21,24 @@ AddPackage --foreign paru-debug # Detached debugging symbols for paru
 CopyFile /boot/loader/entries/arch-fallback.conf 755
 CopyFile /boot/loader/entries/arch.conf 755
 CopyFile /boot/loader/loader.conf 755
-CopyFile /etc/conf.d/snapper
 CopyFile /etc/fstab
 CopyFile /etc/hostname
 CopyFile /etc/locale.conf
 CopyFile /etc/locale.gen
 CreateLink /etc/localtime /usr/share/zoneinfo/Asia/Kolkata
-CopyFile /etc/pacman.d/mirrorlist
-CopyFile /etc/snapper/configs/root 640
 CopyFile /etc/sudoers.d/wheel 440
+
+CopyFile /etc/conf.d/snapper
+CopyFile /etc/snapper/configs/root 640
+
+# network
 CopyFile /etc/systemd/network/20-ethernet.network
 CopyFile /etc/systemd/resolved.conf.d/dns_servers.conf
 CopyFile /etc/systemd/resolved.conf.d/mdns.conf
 CopyFile /etc/systemd/resolved.conf.d/noresolved.conf
 CreateLink /etc/systemd/system/dbus-org.freedesktop.network1.service /usr/lib/systemd/system/systemd-networkd.service
 CreateLink /etc/systemd/system/dbus-org.freedesktop.resolve1.service /usr/lib/systemd/system/systemd-resolved.service
+
+# reflector
+CreateLink /etc/systemd/system/multi-user.target.wants/reflector.service /usr/lib/systemd/system/reflector.service
+CopyFile /etc/xdg/reflector/reflector.conf
