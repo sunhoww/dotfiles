679240dcef692d1d8c61c31eae2d38441b302aab
Sun Howworngbum <sun@libermatic.com>
2025-01-18 00:34:51 +0530

    feat: enable timesync

--- 20-base.sh
+++ 20-base.sh
@@ -48,3 +48,11 @@ CopyFile /etc/systemd/resolved.conf.d/mdns.conf
 CopyFile /etc/systemd/resolved.conf.d/noresolved.conf
 CreateLink /etc/systemd/system/dbus-org.freedesktop.network1.service /usr/lib/systemd/system/systemd-networkd.service
 CreateLink /etc/systemd/system/dbus-org.freedesktop.resolve1.service /usr/lib/systemd/system/systemd-resolved.service
+
+# timesync
+CreateLink /etc/systemd/system/dbus-org.freedesktop.timesync1.service /usr/lib/systemd/system/systemd-timesyncd.service
+CreateLink /etc/systemd/system/sysinit.target.wants/systemd-timesyncd.service /usr/lib/systemd/system/systemd-timesyncd.service
+CreateFile /var/lib/systemd/timesync/clock '' systemd-timesync systemd-timesync >/dev/null
+
+SetFileProperty /var/lib/systemd/timesync group systemd-timesync
+SetFileProperty /var/lib/systemd/timesync owner systemd-timesync
