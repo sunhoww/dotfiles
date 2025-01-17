120889cfe990895ad521557e0bfb4228f374f279
Sun Howworngbum <sun@libermatic.com>
2025-01-17 20:58:40 +0530

    refactor: section pkgs and files

--- 60-development.sh
+++ 60-development.sh
@@ -5,9 +5,12 @@ AddPackage neovim               # Fork of Vim aiming to improve user experience,
 AddPackage starship             # The cross-shell prompt for astronauts
 AddPackage ttc-iosevka          # Typeface family designed for coding, terminal use and technical documents.
 AddPackage ttf-iosevkaterm-nerd # Patched font Iosevka Term from nerd fonts library
-AddPackage fd                   # Simple, fast and user-friendly alternative to find
-AddPackage fzf                  # Command-line fuzzy finder
-AddPackage lazygit              # Simple terminal UI for git commands
-AddPackage ripgrep              # A search tool that combines the usability of ag with the raw speed of grep
+
+# fzf
+AddPackage fzf # Command-line fuzzy finder
 
 IgnorePath '/usr/share/vim/vimfiles/doc/tags'
+
+AddPackage fd      # Simple, fast and user-friendly alternative to find
+AddPackage lazygit # Simple terminal UI for git commands
+AddPackage ripgrep # A search tool that combines the usability of ag with the raw speed of grep
