417602a88b9261f6f714da23576183dec755d5a0
Sun Howworngbum <sun@libermatic.com>
2025-01-22 18:28:55 +0530

    feat: add inkscape

--- 61-apps.sh
+++ 61-apps.sh
@@ -1,6 +1,7 @@
 # Fri Jan 17 07:45:52 PM IST 2025 - graphical apps
 
-AddPackage firefox # Fast, Private & Safe Web Browser
+AddPackage firefox  # Fast, Private & Safe Web Browser
+AddPackage inkscape # Professional vector graphics editor
 
 AddPackage --foreign google-chrome # The popular web browser by Google (Stable Channel)
 
