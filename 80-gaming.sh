70acdc543fe858a842ba490731101c539b65178b
Sun Howworngbum <sun@libermatic.com>
2025-02-11 13:16:07 +0530

    fix: ignore steam files

--- 80-gaming.sh
+++ 80-gaming.sh
@@ -4,6 +4,7 @@ AddPackage lib32-systemd # system and service manager (32-bit)
 AddPackage steam         # Valve's digital software delivery system
 
 IgnorePath '/usr/lib/gtk-4.0/*'
+IgnorePath '/usr/lib/gconv/gconv-modules.cache'
 IgnorePath '/usr/lib32/gconv/gconv-modules.cache'
 
 CopyFile /etc/sysctl.d/80-gamecompatibility.conf
