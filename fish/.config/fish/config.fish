4ca566192c3b5b2dd1e20a6d4f1834b63a81eb0f
Sun Howworngbum <sun@libermatic.com>
2025-03-27 07:25:25 +0530

    refactor: remove unused configs

--- fish/.config/fish/config.fish
+++
@@ -1,4 +0,0 @@
-set -gx PATH "$HOME/.local/bin" $PATH
-
-# The next line updates PATH for the Google Cloud SDK.
-if [ -f "$HOME/.local/opt/google-cloud-sdk/path.fish.inc" ]; . "$HOME/.local/opt/google-cloud-sdk/path.fish.inc"; end
--- zsh/README.md
+++
@@ -1,5 +0,0 @@
-To install a new plugin or modify plugins
-
-1. add plugin to *~/.zsh_plugins.txt*
-2. `rm ~/.zsh_plugins.sh`
-3. re-open terminal
