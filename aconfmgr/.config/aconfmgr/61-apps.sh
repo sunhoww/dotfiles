c321f86d646437327df14a0f655e6331c3c247ea
Sun Howworngbum <sun@libermatic.com>
2025-05-23 00:13:15 +0530

    feat: add obs with virtual camera

--- aconfmgr/.config/aconfmgr/61-apps.sh
+++ aconfmgr/.config/aconfmgr/61-apps.sh
@@ -18,3 +18,12 @@ AddPackage freecad                   # Feature based parametric 3D CAD modeler
 AddPackage openscad                  # The programmers solid 3D CAD modeller
 AddPackage --foreign cura-bin        # State-of-the-art slicer app to prepare your 3D models for your 3D printer
 AddPackage --foreign orca-slicer-bin # G-code generator for 3D printers
+
+# Thu May 22 11:59:22 PM IST 2025 - OBS
+
+AddPackage linux-headers      # Headers and scripts for building modules for the Linux kernel
+AddPackage v4l2loopback-dkms  # v4l2-loopback device – module sources
+AddPackage v4l2loopback-utils # v4l2-loopback device – utilities only
+AddPackage obs-studio         # Free, open source software for live streaming and recording
+
+IgnorePath '/var/lib/dkms'
